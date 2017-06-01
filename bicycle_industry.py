c98515f16bda1ec7b9e4dd1c26adc8d335591484
rcdc1989 <raphaelcapon@gmail.com>
2017-06-01 14:48:52 +0000

    continuing work on bicycle industry git status

--- bicycle_industry.py
+++ bicycle_industry.py
@@ -6,9 +6,9 @@ class Wheel(object):
         
         #wheel data lists name, weight (kg) and cost
         #for each wheel type
-        wheel_data = {"a":["el cheapo",1,10], 
-                      "b":["classic",1.5,25],
-                      "c":["royal",0.75,100]
+        wheel_data = {"a":["el cheapo",1,30], 
+                      "b":["classic",1.5,55],
+                      "c":["royal",0.75,120]
                      }
         self.wheel_type = wheel_type 
         self.name = wheel_data[wheel_type][0]
@@ -29,8 +29,8 @@ class Frame(object):
         #wheel data lists name, weight (kg) and cost
         #for each frame type
         frame_data = {"a":["woody",17,50],
-                      "b":["steely",15,75],
-                      "c":["carbon-fibery",8,300]
+                      "b":["steely",15,150],
+                      "c":["carbon-fibery",8,900]
                      }
         self.frame_type = frame_type 
         self.name = frame_data[frame_type][0]
@@ -56,8 +56,6 @@ class Bicycle(object):
         self.cost = (wheels[0].get_cost() + 
                      wheels[1].get_cost() +
                      frame.get_weight())
-        
-     
     #accessors  
     def get_name(self):
         return self.name
@@ -75,24 +73,49 @@ class Bikeshop(object):
         self.name=name
         self.inventory=[]
         self.margin = 0.2
-        self.sales = 0
+        self.profit = 0
     
+    #method of sellinf bicycle to customer
+    def sell(self, bicycle, customer):
+        price = bicycle.get_cost*(1+self.margin)
+        #customer.buy returns true if they can afford it
+        if customer.buy(bicycle,price):
+            #update profit leger
+            self.profit = self.profit + bicycle.get_cost * self.margin
+            #use list comprehension to remove from inventory
+            self.inventory = [unit for unit in self.inventory 
+                              if unit is not bicycle]
+                              
+    #method for adding bicycle to inventory
+    def stock(self, bicycle):
+        self.inventory.append(bicycle)
     #accessors
     def get_name(self):
         return self.name
-        
-    def sell(self,bicycle,customer):
-        if customer.get_budget
-        
+    def get_profit(self):
+        return self.profit
+    def get_inventory(self):
+        return self.inventory
+            
 class Customer(object):
     def __init__(self,name, budget):
         self.name = name
         self.budget = budget
         self.owned_bikes = []
         
-    def buy(self,bicyle,price):
+    def buy(self,bicycle,price):
         
-    
+        if self.budget >= price:
+            self.budget = self.budget - price
+            self.owned_bikes.append(bicycle)
+            print(str(self.name) + " bought " +
+                  bicycle.get_name + " for " +
+                  str(price))
+            return True
+        else:
+            print("can't afford it")
+            return False
+            
     #accessors   
     def get_name(self):
         return self.name
@@ -102,27 +125,57 @@ class Customer(object):
         return self.owned_bikes
 
 
+###THIS IS OUR "MAIN" TESTING METHOD FOR NOW
 if __name__ == '__main__':
     
-    #create instances of our objects
+    #create bike instances to stock our shop
     n1 = Wheel("a")
     n2 = Wheel("a")
     nf = Frame("a")
     the_neptune = Bicycle("the_neptune", [n1, n2], nf)
+    
+    b1 = Wheel("a")
+    b2 = Wheel("a")
+    bf = Frame("a")
+    the_badger = Bicycle("the_badger", [b1, b2], bf)
     
     w1 = Wheel("b")
     w2 = Wheel("b")
     wf = Frame("b")
     the_winston = Bicycle("the_winston", [w1, w2], wf)
     
+    s1 = Wheel("b")
+    s2 = Wheel("b")
+    sf = Frame("b")
+    the_stanley = Bicycle("the_stanley", [s1, s2], sf)
+    
     r1 = Wheel("c")
     r2 = Wheel("c")
     rf = Frame("c")
     the_rolls = Bicycle("the_rolls", [r1, r2], rf)
     
-    bob = Customer("bob", 1000)
-
+    q1 = Wheel("c")
+    q2 = Wheel("c")
+    qf = Frame("c")
+    the_queen = Bicycle("the_queen", [q1, q2], qf)
+    
+    #instantiate a shop and stock it with the bikes we created
     a_shop = Bikeshop("a_shop")
+    a_shop.stock(the_queen)
+    a_shop.stock(the_rolls)
+    a_shop.stock(the_stanley)
+    a_shop.stock(the_winston)
+    a_shop.stock(the_badger)
+    a_shop.stock(the_neptune)
+    
+    
+    
+    bob = Customer("Bob", 1000)
+    joe = Customer("Joe",500)
+    ted = Customer("Ted",200)
+    
+
+    
     
     print(str(the_neptune.get_name()))
     print(the_neptune.get_weight())
